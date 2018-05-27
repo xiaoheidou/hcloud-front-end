2c2efca6cbfb241b44b8183bbc478c3f5d12a346
harry827 <langzhi@jiankongbao.com>
2018-05-27 23:41:45 +0800

    fetch添加url参数（params）设置

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -2,13 +2,15 @@
  * @Author: harry.lang 
  * @Date: 2018-04-17 23:25:26 
  * @Last Modified by: harry.lang
- * @Last Modified time: 2018-05-20 11:37:36
+ * @Last Modified time: 2018-05-27 22:44:28
  * fetch参考：https://developer.mozilla.org/zh-CN/docs/Web/API/Fetch_API/Using_Fetch
  */
 import 'whatwg-fetch';
+const _toString = Object.prototype.toString;
 
 export default function _fetch(url, options) {
     options = {
+        method: 'GET',
         //跨域请求参数
         mode: 'cors',
         headers: {
@@ -20,6 +22,14 @@ export default function _fetch(url, options) {
 
     if (options.body && typeof options.body == 'object') {
         options.body = JSON.stringify(options.body);
+    }
+
+    if (_toString.call(options.params) === '[object Object]') {
+        const paramsArray = [];
+        for (let k in options.params) {
+            paramsArray.push(`${k}=${options.params[k]}`);
+        }
+        url = `${url}?${paramsArray.join('&')}`;
     }
 
     // console.log(options);
